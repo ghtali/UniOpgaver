216b791b97300b3f2d5f749a7e33e32905c9b10e
Ali.Ghodrat <ght.ali@hotmail.com>
2016-09-11 22:18:39 +0200

    Program password done!

--- Opgaver/src/Opgaver_03/Password.java
+++ Opgaver/src/Opgaver_03/Password.java
@@ -8,6 +8,16 @@ public class Password {
 		// TODO Auto-generated method stub
 		
 		Scanner scanner = new Scanner(System.in);
+		String input = null;
+		while (true){
+		input = scanner.nextLine();
+		if(input.length()<= 4){
+			System.out.println("Password for kort");
+		}else if(input.length()>= 9){
+			System.out.println("Password for langt");
+		}else{System.out.println("Password tilpas");break;}
+		}
+		scanner.close();
 		
 		
 
