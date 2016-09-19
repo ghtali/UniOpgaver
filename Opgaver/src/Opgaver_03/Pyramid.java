1085561ca466ec56ed4f6510d0e642dbcf1fe6c7
Ali.Ghodrat <ght.ali@hotmail.com>
2016-09-19 21:36:39 +0200

    Pyramid completed.

--- Opgaver/src/Opgaver_03/Pyramid.java
+++ Opgaver/src/Opgaver_03/Pyramid.java
@@ -6,9 +6,27 @@ public class Pyramid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner scanner = new Scanner(System.in);
 
-	}
+		int r,t,c,n;
+        Scanner scanner =new Scanner(System.in);
+        n=scanner.nextInt();
+        t = n;
+        System.out.println("Pyramid "+n);
+        for ( r = 1 ; r <= n ; r++ )//for-loop til pyramider. 
+           {
+              for ( c = 1 ; c < t ; c++ )
+                System.out.print(" ");//for-loop til  antal mellemrum.
+
+              t--;
+
+              for ( c = 1 ; c <= 2*r - 1 ; c++ )
+            	  
+                  System.out.print("#");
+
+              System.out.println("");
+           }
+        scanner.close();
+        
+    }
 
 }
