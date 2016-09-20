7d2a1cf131cbd6914df4e11f3bead9c7727cfbe3
Ali.Ghodrat <ght.ali@hotmail.com>
2016-09-20 13:02:51 +0200

    Opgaver 4 begyndt. Trekant initiating.

---
+++ Opgaver/src/Opgaver_04/Trekant.java
@@ -0,0 +1,14 @@
+package Opgaver_04;
+
+import java.util.Scanner;
+
+public class Trekant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System scanner = new Scanner(System.in);
+
+	}
+
+}
